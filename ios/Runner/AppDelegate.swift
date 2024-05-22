2f4caad2cb9354d185d9799687d86bb6235ebe2d
Hyormone <ddf998@gmail.com>
2024-05-22 14:31:09 +0900

    :sparkles: 네이버 맵 api 설정

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import NMapsMap
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,22 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let methodChannel = FlutterMethodChannel(name: "flutter/app",
+                                              binaryMessenger: controller.binaryMessenger)
+    
+    methodChannel.setMethodCallHandler { (call: FlutterMethodCall, result: FlutterResult) -> Void in
+      if call.method == "setEnvironmentVariables" {
+        if let args = call.arguments as? [String: Any] {
+          if let clientId = args["NAVER_CLIENT_ID"] as? String {
+            UserDefaults.standard.set(clientId, forKey: "NAVER_CLIENT_ID")
+            NMFAuthManager.shared().clientId = clientId
+          }
+        }
+        result(nil)
+      }
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+const String NAVER_CLIENT_ID = dotenv.env['NAVER_CLIENT_ID']!;
+const String NAVER_CLIENT_SECRET = dotenv.env['NAVER_CLIENT_SECRET']!;
