bc69e9d1df25cf41d21582620cddce9ef2348fdb
Hyormone <ddf998@gmail.com>
2024-05-27 09:45:29 +0900

    :bug: 빌드 에러 관련 수정

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -1,10 +1,14 @@
-﻿//
+//
 //  Generated file. Do not edit.
 //
 
 import FlutterMacOS
 import Foundation
 
+import geolocator_apple
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
